9de944566532fec4285520ef396c34811333df3e
TheRyuzaki <lod.skot@gmail.com>
2019-09-23 00:45:39 +0400

    Maybe finish download engine =(

--- install.php
+++ install.php
@@ -15,14 +15,34 @@ if (is_dir('./engine') == false) {
             file_put_contents('./EasyCMS.zip', fopen('https://github.com/iamryuzaki/EasyCMS/archive/master.zip', 'r'));
             echo PHP_EOL . '<br>[' . date('H:i:s') . '] Download has been completed, start unzip...';
 
-            $zip = new ZipArchive;
-            $zip->open('./EasyCMS.zip');
-            $zip->extractTo('./', array($zip->getNameIndex(0)['basename']));
-            $zip->close();
+            if (class_exists('ZipArchive')) {
+                $folderName = '';
+                $zip = new ZipArchive;
+                $zip->open('./EasyCMS.zip');
+                if ($zip->numFiles > 0) {
+                    $folderName = basename($zip->getNameIndex(0));
+                    $zip->extractTo('./');
+                    $zip->close();
+                    echo PHP_EOL . '<br>[' . date('H:i:s') . '] Unzip has been completed, start move...';
+                    $files = scandir('./' . $folderName);
+                    foreach ($files as $item) {
+                        if ($item != '.' && $item != '..') {
+                            rename('./' . $folderName . '/' . $item, './' . $item);
+                        }
+                    }
+                    @unlink('./' . $folderName);
+                } else {
+                    $zip->close();
+                    echo PHP_EOL . '<br>[' . date('H:i:s') . '] Error unzip, archive is empty!';
+                }
+            } else
+            {
+                echo PHP_EOL . '<br>[' . date('H:i:s') . '] Error unzip, not found class ZipArchive! Please unzip EasyCMS.zip arhive, after use <a href="/install.php">next step</a>!';
+            }
         } catch (\Throwable $ex) {
+            die(PHP_EOL . '<br>[' . date('H:i:s') . '] Exception! <a href="/install.php?start_install">Maybe reset?</a>?<br>' . PHP_EOL . $ex);
         }
-
-        die(PHP_EOL . '<br>[' . date('H:i:s') . '] Downloading has been completed! <a href="/install.php">Next step</a>?');
+        die(PHP_EOL . '<br>[' . date('H:i:s') . '] Finish! <a href="/install.php">Next step</a>?');
     } else
         die('EasyCMS not found, <a href="?start_install">start install</a>?');
 }
