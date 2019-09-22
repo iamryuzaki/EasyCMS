72ccb4f3129668f773528a5e12b960241f87d905
TheRyuzaki <lod.skot@gmail.com>
2019-09-22 14:36:29 +0400

    Remove rude code

--- index.php
+++ index.php
@@ -3,8 +3,5 @@ require './engine/config.php';
 require './engine/bootstrap.php';
 
 if (Bootstrap::InitApp()) {
-    $plugins = PluginManager::GetRepositoryPlugins('iamryuzaki/EasyCMS-Plugins2');
-    header('Content-Type: text/plain');
-    print_r($plugins);
-    PluginManager::DownloadPlugin('DemoPlugin');
+
 }
